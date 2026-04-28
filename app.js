3d4e7a3d78c0fbdd406dbdafbc430879d3e79435
Zobia Khan <khanzobia279@gmail.com>
2026-04-28 17:11:20 +0500

    Enhance fuelChecker function with input validation
    Added input validation for fuel input to check for empty or null values.

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-function fuelChecker(){ let fuel = document.getElementById("fuelInput").value.toLowerCase(); let message = document.getElementById("message"); 
-       if(fuel < 0.25)
-              { message.innerHTML = "Please refill the fuel"; }else { message.innerHTML = "Don't need to refill" } }
+function fuelChecker() {
+  let fuel = document.getElementById("fuelInput").value;
+  let message = document.getElementById("message");
+
+  if (fuel === "" || fuel === null) {
+    message.innerHTML = "Please enter value in litre";
+  } 
+  else if (fuel < 0.25) {
+    message.innerHTML = "Please refill the fuel";
+  } 
+  else {
+    message.innerHTML = "Don't need to refill";
+  }
+}
