25521b45c9cab0fa457852585804575893af18fb
Zobia Khan <khanzobia279@gmail.com>
2026-04-29 17:01:17 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 function fuelChecker() {
-  let fuel = document.getElementById("fuelInput").value;
+  let fuel = Number(document.getElementById("fuelInput").value);
   let message = document.getElementById("message");
 
-  if (fuel === "" || fuel === null) {
-    message.innerHTML = "Please enter value in litre";
-  } 
-  else if (fuel < 0.25) {
+  if (document.getElementById("fuelInput").value === "") {
+    message.innerHTML = "Please enter a value in litres";
+  } else if (fuel < 0.25) {
     message.innerHTML = "Please refill the fuel";
-  } 
-  else {
+  } else {
     message.innerHTML = "Don't need to refill";
   }
+}
+
+function resetForm() {
+  document.getElementById("fuelInput").value = "";
+  document.getElementById("message").innerHTML = "";
 }
